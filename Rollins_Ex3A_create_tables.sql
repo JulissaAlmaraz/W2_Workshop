e78a6223ae0ba6027a067ea9e9f13199444d62d4
ariel rollins <arollins@my.yearupunited.org>
2026-04-16 15:32:23 -0500

    add rollinns_Ex3A-create_tables.sql

--- Rollins_Ex3A_create_tables.sql
+++ Rollins_Ex3A_create_tables.sql
@@ -1,3 +1,35 @@
 DROP SCHEMA IF EXISTS lana_dog_walking;
 CREATE SCHEMA lana_dog_walking;
 USE lana_dog_walking;
+
+SELECT `customer`.`customer_id`,
+    `customer`.`name`,
+    `customer`.`phone`,
+    `customer`.`email`,
+    `customer`.`address`
+FROM `lana_dog_walking`.`customer`;
+SELECT `dog`.`dog_id`,
+    `dog`.`pet_name`,
+    `dog`.`pet_breed`,
+    `dog`.`age`,
+    `dog`.`special_notes`,
+    `dog`.`customer_id`
+FROM `lana_dog_walking`.`dog`;
+SELECT `payment`.`payment_id`,
+    `payment`.`amount`,
+    `payment`.`date`,
+    `payment`.`method`,
+    `payment`.`walk_id`
+FROM `lana_dog_walking`.`payment`;
+SELECT `walk`.`walk_id`,
+    `walk`.`date`,
+    `walk`.`walk_start_time`,
+    `walk`.`walk_end_time`,
+    `walk`.`status`,
+    `walk`.`dog_id`,
+    `walk`.`walker_id`
+FROM `lana_dog_walking`.`walk`;
+SELECT `walker`.`walker_id`,
+    `walker`.`name`,
+    `walker`.`phone`
+FROM `lana_dog_walking`.`walker`;
