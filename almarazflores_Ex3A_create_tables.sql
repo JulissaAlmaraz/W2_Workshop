ef248146d2a1ebd4db96ac815677fe276949e547
julissa almarazflores <jalmarazflores@my.yearupunited.org>
2026-04-16 15:20:17 -0500

    Added script and changed some columns in excel

--- almarazflores_Ex3A_create_tables.sql
+++ almarazflores_Ex3A_create_tables.sql
@@ -1,3 +1,47 @@
 DROP SCHEMA IF EXISTS lana_dog_walking;
 CREATE SCHEMA lana_dog_walking;
 USE lana_dog_walking;
+
+CREATE TABLE Clients (
+    ClientID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    PhoneNumber VARCHAR(20),
+    Address VARCHAR(255)
+);
+
+CREATE TABLE Dogs (
+    DogID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(50),
+    Breed VARCHAR(50),
+    Notes TEXT,
+    ClientID INT,
+    FOREIGN KEY (ClientID) REFERENCES Clients(ClientID)
+);
+
+CREATE TABLE Walks (
+    WalkID INT AUTO_INCREMENT PRIMARY KEY,
+    Date DATE,
+    Time TIME,
+    Duration INT,
+    Status VARCHAR(50)
+);
+
+CREATE TABLE Payment (
+    PaymentID INT AUTO_INCREMENT PRIMARY KEY,
+    ClientID INT,
+    WalkID INT,
+    Amount DECIMAL(10,2),
+    PaymentMethod VARCHAR(50),
+    Date DATE,
+    FOREIGN KEY (ClientID) REFERENCES Clients(ClientID),
+    FOREIGN KEY (WalkID) REFERENCES Walks(WalkID)
+);
+
+CREATE TABLE WalkDogs (
+	WalkDogID INT AUTO_INCREMENT PRIMARY KEY,
+    WalkID INT,
+    DogID INT,
+    FOREIGN KEY (WalkID) REFERENCES Walks(WalkID),
+    FOREIGN KEY (DogID) REFERENCES Dogs(DogID)
+    );
